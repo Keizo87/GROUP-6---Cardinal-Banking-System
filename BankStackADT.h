afadc7fff50822b8d213f6d7035986d984e2162f
Keizo87 <140407944+Keizo87@users.noreply.github.com>
2023-10-27 19:32:50 +0800

    Create BankStackADT.h

---
+++ BankStackADT.h
@@ -0,0 +1,15 @@
+#ifndef H_BankStackADT
+#define H_BankStackADT
+
+template <class Type>
+class BankStackADT {
+public:
+    virtual void initializeBankStack() = 0;
+    virtual bool isBankStackEmpty() const = 0;
+    virtual bool isBankStackFull() const = 0;
+    virtual void pushToBankStack(const Type& transaction) = 0;
+    virtual Type viewTopTransaction() const = 0;
+    virtual void removeTopTransaction() = 0;
+    virtual void displayRecentTransactions() const = 0; 
+
+#endif
